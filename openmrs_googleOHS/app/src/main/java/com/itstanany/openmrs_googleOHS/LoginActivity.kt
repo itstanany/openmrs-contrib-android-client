35dbc63e9d9c215bfd5b3e115d22ffb3632bdd86
Tanany <itstanany@gmail.com>
2024-03-28 01:08:46 +0200

    Authentication using KeyCload

---
+++ openmrs_googleOHS/app/src/main/java/com/itstanany/openmrs_googleOHS/LoginActivity.kt
@@ -0,0 +1,4 @@
+package com.itstanany.openmrs_googleOHS
+
+// Authentication Logic can be found here at the new project repository
+// https://github.com/itstanany/fhir-app/tree/86fefba42bd79d9e9478e2fe82eb168a0b2418b0/app/src/main/java/com/openmrs/openmrsandroidclient

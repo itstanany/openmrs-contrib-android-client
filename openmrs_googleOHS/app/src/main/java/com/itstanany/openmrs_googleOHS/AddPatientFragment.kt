ae18fa22796da70119d9f893de53f4237b2e0ef6
Tanany <itstanany@gmail.com>
2024-03-28 12:36:49 +0200

    Add patient screen usign google open health stack

---
+++ openmrs_googleOHS/app/src/main/java/com/itstanany/openmrs_googleOHS/AddPatientFragment.kt
@@ -0,0 +1,5 @@
+package com.itstanany.openmrs_googleOHS
+
+// Using Google open Health stack, we can add patient using FHIR standard json format
+// you can find patient form for patient registeration here implemented by me @itsTanany
+// https://github.com/itstanany/fhir-app/tree/edb14dfefc85e5d33bf0b728860c804660c132bc
